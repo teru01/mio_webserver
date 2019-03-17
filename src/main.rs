b5511478c523c560020c6721240bc31a77b8d985
teruya <abas.trace.on@gmail.com>
2019-03-17 16:35:14 +0900

    refoctor

--- src/main.rs
+++ src/main.rs
@@ -32,24 +32,15 @@ impl TCPServer {
 
     fn run(&mut self) -> Result<(), Error> {
         let server = TcpListener::bind(&self.address).expect("Failed to bind address");
-        // linuxなのでバックエンドにはepollシステムコールを利用
         let poll = Poll::new().unwrap();
 
-        // edgeトリガーモード：監視対象のEventdで変更が発生した場合にイベント配信する
-        // レベルトリガー：変更がなくてもイベント？
-
-        // イベントの種類：readyness: どの種類の操作を実行する準備ができているか　readable()は読み取り準備できていることを示す
-        // &serverはEventdを実装している。状態変化するとpollの呼び出してredinessイベントを返す
-        //TOKENでhandleを識別
-        // 第3引数で指定したイベントだけを返す
         poll.register(&server, SERVER, Ready::readable(), PollOpt::edge()).unwrap();
 
-        // イベントを詰め込む
         let mut events = Events::with_capacity(1024);
 
+        // イベントループ
         loop {
             //現在のスレッドをブロックしてイベントを待つ。
-            // 一回のpollに対して複数のeventが発行される場合がある。socketが読み書き可能になると1つの読み書き可能または読み可能と書き可能の2つ
             poll.poll(&mut events, None).unwrap();
             for event in &events {
                 match event.token() {
@@ -58,7 +49,7 @@ impl TCPServer {
                         println!("Connection from {}", &remote);
 
                         let token = Token(self.next_connection_id);
-                        poll.register(&stream, token, Ready::readable(), PollOpt::edge());
+                        poll.register(&stream, token, Ready::readable(), PollOpt::edge()).unwrap();
 
                         if let Some(_) = self.connections.insert(self.next_connection_id, stream){
                             // HashMapは既存のキーで値が更新されると更新前の値を返す
@@ -68,23 +59,19 @@ impl TCPServer {
                     }
 
                     Token(conn_id) => {
-                        // let stream = match self.connections.get_mut(&conn_id) {
-                        //     Some(stream) => stream,
-                        //     _ => return Ok(()),
-                        // };
                         if let Some(stream) = self.connections.get_mut(&conn_id) {
-                            TCPServer::handle_connection(stream, &conn_id).expect("http server error");  //ここでselfへの&mutが作られるのでだめ
+                            if let Some(_) = TCPServer::handle_connection(stream, &conn_id).err() {
+                                stream.write("HTTP/1.0 500 Internal Server Error\r\n".as_bytes())?;
+                                stream.write("Server: mio webserver\r\n".as_bytes())?;
+                                stream.write("\r\n".as_bytes())?;
+                                stream.flush()?;
+                            };
                             self.connections.remove(&conn_id);
                         }
-                    }
-
-                    _ => {
-                        unreachable!();
                     }
                 }
             }
         }
-        Ok(())
     }
 
     fn handle_connection(stream: &mut TcpStream, conn_id: &usize) -> Result<(), Error> {
@@ -96,23 +83,29 @@ impl TCPServer {
         }
 
         let http_pattern = Regex::new(r"(.*) (.*) HTTP/1.([0-1])").unwrap();
-        let captures = http_pattern.captures(str::from_utf8(&buffer[..nbytes]).unwrap()).ok_or("invalid request").unwrap();
+        let captures = match http_pattern.captures(str::from_utf8(&buffer[..nbytes]).unwrap()) {
+            Some(cap) => cap,
+            None => {
+                stream.write("HTTP/1.0 400 Bad Request\r\n".as_bytes())?;
+                stream.write("Server: mio webserver\r\n".as_bytes())?;
+                stream.write("\r\n".as_bytes())?;
+                stream.flush()?;
+                return Ok(());
+            }
+        };
 
         let method = captures.get(1).unwrap().as_str();
         let path = &format!("{}{}{}", env::current_dir().unwrap().display(), WEBROOT, captures.get(2).unwrap().as_str());
         let _version = captures.get(3).unwrap().as_str();
 
         if method == "GET" {
-            println!("path: {}", path);
             let file = match fs::File::open(path) {
                 Ok(file) => file,
-                // Error(Err) => {
-                //     stream.write("HTTP/1.0 404 NOT FOUND\r\n".as_bytes())?;
-                //     stream.write("Server: mio webserver\r\n\r\n".as_bytes())?;
-                //     stream.flush()?;
-                //     return Ok(());
-                // }
-                _ => {
+                Err(_) => {
+                    // パーミッションエラーなどもここに含まれるが手抜きしてnot foundにしている
+                    stream.write("HTTP/1.0 404 Not Found\r\n".as_bytes())?;
+                    stream.write("Server: mio webserver\r\n\r\n".as_bytes())?;
+                    stream.flush()?;
                     return Ok(());
                 }
             };
@@ -125,15 +118,16 @@ impl TCPServer {
             stream.write("\r\n".as_bytes())?;
             stream.write(&buf)?;
             stream.flush()?;
-        } else if method == "HEAD" {
         } else {
+            // サポートしていないHTTPメソッド
+            stream.write("HTTP/1.0 501 Not Implemented\r\n".as_bytes())?;
+            stream.write("Server: mio webserver\r\n".as_bytes())?;
+            stream.write("\r\n".as_bytes())?;
+            stream.flush()?;
         }
         Ok(())
     }
 }
-
-
-
 
 fn main() {
     let args:Vec<String> = env::args().collect();
@@ -142,5 +136,5 @@ fn main() {
         std::process::exit(1);
     }
     let mut server = TCPServer::new(&args[1]);
-    server.run();
+    server.run().expect("Internal Server Error.");
 }
